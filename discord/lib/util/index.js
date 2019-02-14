106780852a42b9028c2a8f83df0e6c519e9dcec4
kyoukaya <kyoukaya@gmail.com>
2019-02-14 13:33:49 +1100

    discord: animated emoji handling in fromDiscord

--- discord/lib/util/index.js
+++ discord/lib/util/index.js
@@ -137,7 +137,7 @@ function fromDiscord (message, server) {
       return '@' + ((m && m.name) || '(???)')
     })
     // :emoji:
-    .replace(/<:(\w+):(\d+)>/g, (_, mention) => ':' + mention + ':')
+    .replace(/<(a|):(\w+):(\d+)>/g, (_, mention) => ':' + mention + ':')
   )
 }
 
